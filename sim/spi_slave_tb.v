1c2e6d4f53a09eefe90eda5edeb4a5054712e88f
Zakary Nafziger <worldofzak@gmail.com>
2019-10-08 20:51:38 -0400

    add transmit, and update testbench

--- sim/spi_slave_tb.v
+++ sim/spi_slave_tb.v
@@ -1,7 +1,7 @@
 `timescale 10ns/1ns
 
 module spi_slave_tb #(
-	parameter WIDTH = 4
+	parameter WIDTH = 8
 );
 
 reg clk;
@@ -10,6 +10,7 @@ reg rst;
 initial begin
         clk = 0;
         rst = 1;
+	wr = 0;
         #1;
         rst = 1;
         clk = 1;
@@ -19,11 +20,19 @@ initial begin
         #1;
         clk = 1;
         rst = 0;
+	wr = 1;
+	#1;
+	clk = 0;
+	#1;
+	clk = 1;
+	#1;
+	clk = 0;
+	wr = 0;
         forever #1 clk = ~clk;
 end
 
 reg sclk, mosi, ss, wr;
-reg [WIDTH-1:0] to_tx;
+wire [WIDTH-1:0] to_tx;
 wire [WIDTH-1:0] rxed;
 wire miso;
 
@@ -43,20 +52,24 @@ spi_slave #(
 );
 
 integer idx;
-reg [3:0] to_shift = 4'b1010;
+reg [8:0] to_shift = 9'b101010101;
 
+// test dut RX
 initial begin
 	mosi = 0;
 	sclk = 0;
-	for(idx = 0; idx < 4; idx = idx + 1) begin
+	for(idx = 0; idx < 9; idx = idx + 1) begin
 		sclk = 0;
-		mosi = to_shift[idx];
+		mosi = to_shift[(8)-idx];
 		#17;
 		sclk = 1;
 		#17;
 	end
 	$finish;
 end
+
+//test dut TX
+assign to_tx = 8'b01010101;
 
 initial begin
 	$dumpfile("spi_slave.vcd");
--- src/spi_slave.v
+++ src/spi_slave.v
@@ -14,15 +14,16 @@ module spi_slave #(
 	// SPI interface signals
 	input sclk,
 	input mosi,
-	output miso,
+	output reg miso,
 	input ss,
 
 	// buffer to serialize and transmit
 	input [TXWIDTH-1:0] tx_buffer,
 	input wr,
 
-	// recived bits
-	output reg [RXWIDTH-1:0] rx_buffer
+	// parallelized recived bits
+	output reg [RXWIDTH-1:0] rx_buffer,
+	output reg rx_dv
 );
 
 wire ah_ss;
@@ -31,22 +32,45 @@ assign ah_ss = ~ss;
 reg [TXWIDTH-1:0] txb;
 reg [RXWIDTH-1:0] rxb;
 
-// reset and write buffers
+integer bits_in, bits_out;
+
+// clock synchronization
 always @(posedge clk) begin
 	if (rst) begin
 		rxb <= 0;
 		txb <= 0;
+		rx_buffer <= 0;
+		bits_in <= 0;
+		bits_out <= 0;
+		miso <= 0;
 	end else if (wr) begin
 		txb <= tx_buffer;
+		bits_out <= TXWIDTH;
 	end else begin
 		txb <= txb;
+	end
+	if (bits_in == RXWIDTH) begin
+		rx_dv <= 1;
+		bits_in <= 0;
 		rx_buffer <= rxb;
+	end else begin
+		rx_dv <= 0;
 	end
 end
 
-// recieve SPI bits
+// recieve bits
 always @(posedge sclk) begin
 	rxb <= {rxb[RXWIDTH-2:0], mosi};
+	bits_in <= bits_in + 1;
+end
+
+// transmit bits
+always @(posedge ~sclk) begin
+	if (bits_out != 0) begin
+		miso <= txb[TXWIDTH-1];
+		txb <= {txb[TXWIDTH-2:0], 1'b0};
+		bits_out <= bits_out - 1;
+	end
 end
 
 endmodule
