37e966653e12c4eb8d402e617a8f8edaeac0726a
Zakary Nafziger <worldofzak@gmail.com>
2019-10-16 10:02:31 -0400

    first attempt at implementing command set

--- src/python/spi_test.py
+++ src/python/spi_test.py
@@ -1,9 +1,12 @@
 from machine import SPI, Pin
 import time
-hard_spi = SPI(1, baudrate=5000000, bits=4, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
+hard_spi = SPI(1, baudrate=1000, bits=4, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
 
-def check(byte):
-	hard_spi.write(bytearray([byte]))
-	print("wrote: " + str(hex(byte)))
-	d = hard_spi.read(1)
+def check(dat):
+	hard_spi.write(bytearray([0x01]))
+	hard_spi.write(dat)
+
+	hard_spi.write(bytearray([0x02]))
+	d = hard_spi.read(8)
+
 	print("read: " + str(d))
--- src/spi_test_top.v
+++ src/spi_test_top.v
@@ -16,7 +16,7 @@ module spi_test_top (
   //inout pin19_sclk,
   //inout pin20,
   //inout pin21,
-  inout pin22
+  input pin22
 );
 
 wire clk, rst;
@@ -28,17 +28,11 @@ assign mosi = pin2;
 assign miso = pin3;
 assign ss = pin4;
 
-wire [7:0] loopback;
 wire rx_dv;
-reg wr;
-
-always @(posedge clk) begin
-	if (rx_dv && (loopback != 8'b00000000)) begin
-		wr <= 1;
-	end else begin
-		wr <= 0;
-	end
-end
+wire tx_halt;
+wire wr;
+wire [7:0] data_out;
+wire [7:0] data_in;
 
 OSCH #(
 	.NOM_FREQ("53.2")
@@ -57,10 +51,70 @@ spi_slave #(
 	.mosi(mosi),
 	.miso(miso),
 	.ss(ss),
-	.tx_buffer(loopback),
+	.tx_buffer(data_out),
 	.wr(wr),
-	.rx_buffer(loopback),
+	.tx_halt(tx_halt),
+	.rx_buffer(data_in),
 	.rx_dv(rx_dv)
+);
+
+wire cmd_vld;
+wire [7:0] cmd;
+assign cmd_vld = (rx_dv && ((data_in == 8'h01) || (data_in == 8'h02)))? 1'b1: 1'b0;
+
+wire [63:0] d_dat;
+wire [63:0] q_dat;
+wire d_we;
+
+register #(
+	.WIDTH(8)
+) cmd_reg (
+	.clk(clk),
+	.rst(rst),
+	.we(cmd_vld),
+	.d(data_in),
+	.q(cmd)
+);
+
+register #(
+	.WIDTH(64)
+) data_reg (
+	.clk(clk),
+	.rst(rst),
+	.we(d_we),
+	.d(d_dat),
+	.q(q_dat)
+);
+
+wire s2p_we;
+assign s2p_we = ((cmd == 8'h01) && rx_dv)? 1'b1: 1'b0;
+
+SIPO #(
+	.DEPTH(8),
+	.WIDTH(8)
+) s2p (
+	.clk(clk),
+	.rst(rst),
+	.si(data_in),
+	.we(s2p_we),
+	.po(d_dat),
+	.po_dv(d_we)
+);
+
+wire p2s_we;
+assign p2s_we = ((cmd == 8'h02) && (rx_dv))? 1'b1: 1'b0;
+
+PISO #(
+	.DEPTH(8),
+	.WIDTH(8)
+) p2s (
+	.clk(clk),
+	.rst(rst),
+	.pi(q_dat),
+	.we(p2s_we),
+	.so(data_out),
+	.so_dv(wr),
+	.halt(tx_halt)
 );
 
 endmodule
