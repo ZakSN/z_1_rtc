70ce10d9b741a7ea89594802d4322891c62518be
Zakary Nafziger <worldofzak@gmail.com>
2021-07-09 19:58:52 -0400

    add logging to the python script since tee didn't want to behave

--- software/z_1_rtc_host.py
+++ software/z_1_rtc_host.py
@@ -3,6 +3,7 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyUSB0')
+logfile = open("error_log.txt", "w")
 
 def get_z_1_rtc_epoch():
 	no_epoch = True
@@ -23,15 +24,22 @@ def get_z_1_rtc_epoch():
 			pass
 	return z_1_rtc_epoch
 
+print("normalized unix epoch [s], accumulated error [s]")
+logfile.write("normalized unix epoch [s], accumulated error [s]" + "\n")
+
 z_epoch_start = get_z_1_rtc_epoch()
 u_epoch_start = int(time.time())
 
-print("starting epoch [s]: " + str(u_epoch_start))
-print("normalized unix epoch [s], accumulated error [s]")
 while(True):
-	unix_epoch = int(time.time())
-	z_1_rtc_epoch = get_z_1_rtc_epoch()
-	normalized_z_epoch = z_1_rtc_epoch - z_epoch_start
-	normalized_u_epoch = unix_epoch - u_epoch_start
-	accumulated_error = normalized_z_epoch - normalized_u_epoch
-	print(str(normalized_u_epoch) + ", " + str(accumulated_error))
+	try:
+		unix_epoch = int(time.time())
+		z_1_rtc_epoch = get_z_1_rtc_epoch()
+		normalized_z_epoch = z_1_rtc_epoch - z_epoch_start
+		normalized_u_epoch = unix_epoch - u_epoch_start
+		accumulated_error = normalized_z_epoch - normalized_u_epoch
+		print(str(normalized_u_epoch) + ", " + str(accumulated_error))
+		logfile.write(str(normalized_u_epoch) + ", " + str(accumulated_error) + "\n")
+	except KeyboardInterrupt:
+		print("\nclosing log")
+		logfile.close()
+		exit()
