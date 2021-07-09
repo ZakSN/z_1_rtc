d72f231c6736a35bb081f2e9e2b106c50cc407ca
Zakary Nafziger <worldofzak@gmail.com>
2021-07-09 17:04:12 -0400

    modify project to work with lmcxo2-2000hc
    also some reorganization of file structure
    	- place verilog sim and src in its own dir
    	- create software dir

--- verilog/src/z_1_rtc_top.v
+++ verilog/src/z_1_rtc_top.v
@@ -2,60 +2,32 @@ module z_1_rtc_top (
 	`ifdef SIM
 		input sim_clk,
 	`endif
-	//***SPI0 BUS***
-	inout  pin11, // SPI0_CLK
-	inout  pin4,  // SPI0_MOSI
-	inout pin20, // SPI0_MISO
-	input  pin18, // SPI0_CS/b_ATMEGA328PB_RST
-	
-	//***EPOCH CNTRL***
-	input  pin1,  // EPOCH_S
-	input  pin2,  // EPOCH_R
-
-	//***MFREQ & RST***
-	input  pin5,  // MASTER_FREQ
-	input  pin22, // b_FPGA_RST
-	
-	//***OUTPUT PINS***
-	//inout pin6,  // out1 (TIMER_S)
-	//inout pin7,  // out2 (TIMER_R)
-	output pin19, // out3 (half_hz_50)
-	//output pin21, // out4
-	output pin3  // out5
-	//inout pin8,  // out6
-	//inout pin9,  // out7
-	//inout pin10, // out8
-	//inout pin16, // out9
-	//inout pin17, // out10
-	
-	//***UNUSED***
-	//PRGM pin12,
-	//PRGM pin13,
-	//PRGM pin14,
-	//PRGM pin15,
+	             //micro pins  //usage
+	output pb8b, //pc0 ------> miso
+	input  pb8a, //pc1 ------> sck
+	input  pb5a, //pe2 ------> ss
+	input  pb25b,//pe3 ------> mosi
+	inout  pl9a, //pb1 ------> n/a
+	output pl2a, //n/a ------> one_hz
+	input  pl2b  //n/a ------> freq source
 );
 
 // global clk and rst
 wire clk, rst;
-assign rst = ~pin22;
+assign rst = 1'b0;
 
 // SPI0 interface
 wire sclk, mosi, miso, ss;
-assign ss = pin18;
-
-assign pin11 = ss ? pin11 : 1'bz;
-assign sclk = pin11;
-
-assign pin4 = ss ? pin4 : 1'bz;
-assign mosi = pin4;
-
-assign pin20 = ss ? miso : 1'bz;
+assign ss = ~pb5a;
+assign mosi = pb25b & ss;
+assign pb8b = miso; //ss ? miso : 1'bz;
+assign sclk = pb8a & ss;
 
 // other external pins
 wire frequency_source, epoch_set, epoch_reset;
-assign frequency_source = pin5;
-assign epoch_set = pin1;
-assign epoch_reset = pin2;
+assign frequency_source = pl2b;
+//assign epoch_set = pin1;
+//assign epoch_reset = pin2;
 
 // SPI internal signals
 wire [7:0] o_spi_data, i_spi_data;
@@ -64,18 +36,19 @@ wire spi_we, spi_halt, spi_dv;
 // epoch signals
 wire [63:0] i_epoch_data, o_epoch_data;
 wire epoch_le, epoch_ce;
+assign epoch_ce = 1'b1;
 
 // divider signals
 wire one_hz;
 wire half_hz_50;
 
 // debug outputs
-assign pin19 = rst;
-assign pin3 = half_hz_50;
+assign pl2a = half_hz_50;
+//assign pin9 = half_hz_50;
 
 `ifndef SIM
 OSCH #(
-	.NOM_FREQ("88.67")
+	.NOM_FREQ("53.2")
 ) internal_oscillator_inst (
 	.STDBY(1'b0),
 	.OSC(clk)
@@ -136,11 +109,11 @@ timer epoch (
 	.o_time(o_epoch_data)
 );
 
-trigger epoch_trigger (
+/*trigger epoch_trigger (
 	.rst(rst),
 	.s(epoch_set),
 	.r(epoch_reset),
 	.q(epoch_ce)
-);
+);*/
 
 endmodule
