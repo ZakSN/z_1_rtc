87f4a4ef807b9be3c5ffbcd59746eae131882cf4
Zakary Nafziger <worldofzak@gmail.com>
2021-07-14 21:40:24 -0400

    nearly working minimum viable firmware/software
    still need to fix display formatting

--- firmware/avr/z_1_rtc_main.c
+++ firmware/avr/z_1_rtc_main.c
@@ -5,7 +5,9 @@
 #include "z_1_rtc_lcd/z_1_rtc_lcd.h"
 #include "z_1_rtc_spi/z_1_rtc_spi.h"
 #include "z_1_rtc_uart/z_1_rtc_uart.h"
-
+#include "stdbool.h"
+#include "time.h"
+#include "util/usa_dst.h"
 
 char nybble_to_hex(unsigned char);
 
@@ -16,6 +18,66 @@ char nybble_to_hex(unsigned char nybble) {
 	}
 	char hex_lut[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 	return hex_lut[nybble];
+}
+
+void uart_send_str(char* to_send) {
+	for(int i=0; to_send[i]!='\0'; i++){
+		USART_Transmit(to_send[i]);
+	}
+}
+
+void uart_receive_str(char* buffer, int buffer_len) {
+	for(int i=0; i<buffer_len; i++){
+		buffer[i] = USART_Receive();
+	}
+}
+
+uint64_t epoch_to_int(char* epoch){
+	uint64_t int_epoch = 0;
+	int_epoch = int_epoch+epoch[0];
+	for (int i=1; i<8; i++) {
+		int_epoch = int_epoch<<8;
+		int_epoch = int_epoch+(unsigned char)epoch[i];
+	}
+	return int_epoch;
+}
+
+void ui64toa(uint64_t to_convert, char* buffer){
+	char converted[19];
+	for (int i=0; i<19; i++) {
+		converted[i] = '0';
+	}
+	int idx = 18;
+	int digit;
+	char digit_to_ascii[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	while(to_convert > 0){
+		digit = to_convert % 10;
+		converted[idx] = digit_to_ascii[digit];
+		to_convert = to_convert / 10;
+		idx--;
+	}
+	uart_send_str("\r\n");
+	for (int i=0; i<19; i++) {
+		buffer[i] = converted[i];
+	}
+	buffer[19] = '\0';
+}
+
+void get_raw_fpga_epoch(char* epoch){
+	// ask the fpga for the current epoch
+	output_low(PORTE, PE2);
+	spi_tranceiver(0x02); // RD CMD
+	for (int i=0; i<8; i++) {
+		epoch[i] = spi_tranceiver(0x0);
+	}
+
+	output_high(PORTE, PE2);
+}
+
+uint64_t get_fpga_epoch(void){
+	char epoch[8];
+	get_raw_fpga_epoch(epoch);
+	return epoch_to_int(epoch);
 }
 
 int main(void) {
@@ -29,73 +91,147 @@ int main(void) {
 	set_output(DDRE, PE2);
 	output_high(PORTE, PE2);
 
-	/*
-	//epoch set
-	set_output(DDRB, PB1);
-	output_high(PORTB, PB1);
-	_delay_ms(50);
-	output_low(PORTB, PB1);
-	*/
-
-	// wait for the crystal to warm up
-	// probably unecessary
 	lcd_send_screen(
-		"FGPA EPOCH:",
-		"starting...",
+		"Cal mode? [y/N]",
+		"\0",
 		"\0",
 		"\0",
 		0x1F
 	);
 
+	bool cal_mode = false;
+	uart_send_str("cal mode? [y/N]");
+	char cal_buffer[1];
+	uart_receive_str(cal_buffer, 1);
+	if (cal_buffer[0] == 'y' || cal_buffer[0] == 'Y') {
+		cal_mode = true;
+	}
+	
+	uint64_t fpga_epoch;
+	time_t y2k_epoch;
+	int UTC_offset = 0;
+	char fmt_str[100];
+	
+	
+	if (!cal_mode) {
+		lcd_send_screen(
+			"\0",
+			"  waiting for epoch",
+			"     from host",
+			"\0",
+			0x1F
+		);
+
+		uart_send_str("64 bit unix epoch> ");
+		char epoch_buffer[8];
+		uart_receive_str(epoch_buffer, 8);
+
+		output_low(PORTE, PE2);
+		spi_tranceiver(0x01); // WR CMD
+		for (int i=0; i<8; i++) {
+			spi_tranceiver(epoch_buffer[i]);
+		}
+		output_high(PORTE, PE2);
+		
+		lcd_send_screen(
+			"\0",
+			"waiting for UTC",
+			"offset from host",
+			"\0",
+			0x1F
+		);
+
+		uart_send_str("8 bit utc offset> ");
+		char utc_offset_buffer[1];
+		uart_receive_str(utc_offset_buffer, 1);
+		UTC_offset = (signed char)utc_offset_buffer[0];
+
+		lcd_send_screen(
+			"\0",
+			"waiting for format",
+			"string from host",
+			"\0",
+			0x1F
+		);
+
+		uart_send_str("format string> ");
+		uart_receive_str(fmt_str, 100);
+	}
 	_delay_ms(1000);
 
 	while (1) {
 		// pause the loop
 		_delay_ms(500);
 
-		// ask the fpga for the current epoch
-		output_low(PORTE, PE2);
-		spi_tranceiver(0x02); // RD CMD
-		char epoch[8] = {
-			spi_tranceiver(0x0), // byte 0
-			spi_tranceiver(0x0),
-			spi_tranceiver(0x0),
-			spi_tranceiver(0x0),
-			spi_tranceiver(0x0),
-			spi_tranceiver(0x0),
-			spi_tranceiver(0x0),
-			spi_tranceiver(0x0) // byte 7
-		};
-		output_high(PORTE, PE2);
+		fpga_epoch = get_fpga_epoch();
+		y2k_epoch = fpga_epoch - (uint64_t)UNIX_OFFSET;
+		
+		y2k_epoch = y2k_epoch + UTC_offset*ONE_HOUR;
 
-		//convert the current epoch to a string that can be displayed
-		char hex_epoch[19] = {'0', 'x', 'F','F','F','F','F','F','F','F','F','F','F','F','F','F','F','F', 0x0};
-		int idxe = 2, idxo = 3; 
-		for (int i=0; i<8; i++){
-			hex_epoch[idxe] = nybble_to_hex((epoch[i]>>4)&0xF);
-			hex_epoch[idxo] = nybble_to_hex(epoch[i]&0xF);
-			idxe+=2;
-			idxo+=2;
+		struct tm* timeptr;
+		timeptr = gmtime(&y2k_epoch);
 
+		char buffer[80];
+
+		strftime(buffer, 80, fmt_str, timeptr);
+		//uart_send_str(buffer);
+		//uart_send_str("\r\n");
+
+		char ln1[21];
+		char ln2[21];
+		char ln3[21];
+		char ln4[21];
+		for (int i=0; i<20; i++){
+			ln1[i] = buffer[i];
+			ln2[i] = buffer[i+20];
+			ln3[i] = buffer[i+40];
+			ln4[i] = buffer[i+60];
 		}
-
-		// print the current epoch on the screen
+		ln1[20] = '\0';
+		ln2[20] = '\0';
+		ln3[20] = '\0';
+		ln4[20] = '\0';
+		
 		lcd_send_screen(
-			"FPGA EPOCH:",
-			hex_epoch,
-			"\0",
-			"\0",
-			0x1C
+			ln1,
+			ln2,
+			ln3,
+			ln4,
+			0x1F
 		);
 
-		//send the current epoch out over uart as a stripped hex number
-		// followed by a newline
-		for (int i=2; i<18; i++){
-			USART_Transmit(hex_epoch[i]);
+		if (cal_mode){
+			char epoch[8];
+			get_raw_fpga_epoch(epoch);
+			//convert the current epoch to a string that can be displayed
+			char hex_epoch[19] = {'0', 'x', 'F','F','F','F','F','F','F','F','F','F','F','F','F','F','F','F', 0x0};
+			int idxe = 2, idxo = 3; 
+			for (int i=0; i<8; i++){
+				hex_epoch[idxe] = nybble_to_hex((epoch[i]>>4)&0xF);
+				hex_epoch[idxo] = nybble_to_hex(epoch[i]&0xF);
+				idxe+=2;
+				idxo+=2;
+
+			}
+
+			// print the current epoch on the screen
+			lcd_send_screen(
+				"FPGA EPOCH:",
+				hex_epoch,
+				"\0",
+				"\0",
+				0x1C
+			);
+		//if (cal_mode) {
+			//send the current epoch out over uart as a stripped
+			//hex number followed by a newline
+			for (int i=2; i<18; i++){
+				USART_Transmit(hex_epoch[i]);
+			}
+			// need to do a carriage return and a newline for
+			// terminal programs to be happy
+			USART_Transmit('\r');
+			USART_Transmit('\n');
 		}
-		// need to do a carriage return and a newline for terminal
-		// programs to be happy
-		USART_Transmit('\r');
-		USART_Transmit('\n');
 	}
 }
--- software/z_1_rtc_host.py
+++ software/z_1_rtc_host.py
@@ -2,10 +2,7 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyUSB0')
-logfile = open("error_log.txt", "w")
-
-def get_z_1_rtc_epoch():
+def get_z_1_rtc_epoch(ser):
 	no_epoch = True
 	while(no_epoch):
 		# get an epoch string out of the clock, strip off all of the extraneous
@@ -24,22 +21,65 @@ def get_z_1_rtc_epoch():
 			pass
 	return z_1_rtc_epoch
 
-print("normalized unix epoch [s], accumulated error [s]")
-logfile.write("normalized unix epoch [s], accumulated error [s]" + "\n")
+def cal_mode(ser):
+	logfile = open("error_log.txt", "w")
 
-z_epoch_start = get_z_1_rtc_epoch()
-u_epoch_start = int(time.time())
+	print("normalized unix epoch [s], accumulated error [s]")
+	logfile.write("normalized unix epoch [s], accumulated error [s]" + "\n")
 
-while(True):
-	try:
-		unix_epoch = int(time.time())
-		z_1_rtc_epoch = get_z_1_rtc_epoch()
-		normalized_z_epoch = z_1_rtc_epoch - z_epoch_start
-		normalized_u_epoch = unix_epoch - u_epoch_start
-		accumulated_error = normalized_z_epoch - normalized_u_epoch
-		print(str(normalized_u_epoch) + ", " + str(accumulated_error))
-		logfile.write(str(normalized_u_epoch) + ", " + str(accumulated_error) + "\n")
-	except KeyboardInterrupt:
-		print("\nclosing log")
-		logfile.close()
-		exit()
+	z_epoch_start = get_z_1_rtc_epoch()
+	u_epoch_start = int(time.time())
+
+	while(True):
+		try:
+			unix_epoch = int(time.time())
+			z_1_rtc_epoch = get_z_1_rtc_epoch()
+			normalized_z_epoch = z_1_rtc_epoch - z_epoch_start
+			normalized_u_epoch = unix_epoch - u_epoch_start
+			accumulated_error = normalized_z_epoch - normalized_u_epoch
+			print(str(normalized_u_epoch) + ", " + str(accumulated_error))
+			logfile.write(str(normalized_u_epoch) + ", " + str(accumulated_error) + "\n")
+		except KeyboardInterrupt:
+			print("\nclosing log")
+			logfile.close()
+			exit()
+
+def main():
+	#TODO
+	ser = serial.Serial('/dev/ttyUSB0')
+	cm = input("Cal Mode [y/N] ")
+	if cm == 'y' or cm == 'Y':
+		ser.write(bytes('y'.encode('ascii')))
+		cal_mode()
+	else:
+		ser.write(bytes('n'.encode('ascii')))
+	
+	time.sleep(2)
+
+	unix_epoch = int(time.time())
+	print(unix_epoch)
+	unix_epoch = bytes.fromhex("%0.16X" % unix_epoch)
+	ser.write(unix_epoch)
+	
+	time.sleep(1)
+
+	utc_offset = time.localtime().tm_gmtoff
+	utc_offset = int(utc_offset/3600)
+	ser.write(utc_offset.to_bytes(1, byteorder='big', signed=True))
+
+	time.sleep(1)
+
+	default_fmt_str = "%A %e %B %Y %I:%M.%S %p"
+	fmt_str = input("enter display format string [leave blank for default] ")
+	if fmt_str == "":
+		fmt_str = default_fmt_str
+	fmt_str = fmt_str
+	while(len(fmt_str) < 100):
+		fmt_str = fmt_str + '\0'
+	print(fmt_str)
+	ser.write(bytes(fmt_str.encode('ascii')))
+	
+	
+
+if __name__ == "__main__":
+	main()
